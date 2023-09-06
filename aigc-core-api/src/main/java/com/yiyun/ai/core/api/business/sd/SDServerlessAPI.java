6202b32a20f1090b8fbcdbe6dd79d1f303c753a1
zh.wang <zh.wang@nio.com>
2023-09-06 15:59:39 +0800

    - update code

--- aigc-core-api/src/main/java/com/yiyun/ai/core/api/business/sd/SDServerlessAPI.java
+++ aigc-core-api/src/main/java/com/yiyun/ai/core/api/business/sd/SDServerlessAPI.java
@@ -7,16 +7,18 @@ import feign.RequestLine;
 
 public interface SDServerlessAPI {
 
+    String CONTENT_TYPE_APPLICATION_JSON = "Content-Type: application/json";
+
     @RequestLine("POST /release/sdapi/v1/img2img")
-    @Headers("Content-Type: application/json")
+    @Headers(CONTENT_TYPE_APPLICATION_JSON)
     SDAny2ImageStruct.SDImage2ImageResponse image2image(SDAny2ImageStruct.SDImage2ImageRequest request);
 
     @RequestLine("POST /release/sdapi/v1/txt2img")
-    @Headers("Content-Type: application/json")
+    @Headers(CONTENT_TYPE_APPLICATION_JSON)
     SDAny2ImageStruct.SDTxt2ImageResponse text2image(SDAny2ImageStruct.SDTxt2ImageRequest sdTxt2ImageRequest);
 
     @Body("{json}")
-    @Headers("Content-Type: application/json")
+    @Headers(CONTENT_TYPE_APPLICATION_JSON)
     @RequestLine("POST /release/sdapi/v1/txt2img")
     SDAny2ImageStruct.SDTxt2ImageResponse text2image(@Param("json") String requestJSON);
 }
